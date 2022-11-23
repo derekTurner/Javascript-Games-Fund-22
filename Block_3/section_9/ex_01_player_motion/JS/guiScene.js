fcb1545f72c7c4818d14fada703bcaa21237a0a5
derek.turner <derek.turner@uws.ac.uk>
2022-11-23 10:42:24 +0000

    refactored

--- Block_3/section_9/ex_01_player_motion/JS/guiScene.js
+++ Block_3/section_9/ex_01_player_motion/JS/guiScene.js
@@ -1,52 +1,32 @@
 import {setSceneIndex}        from "./createScenes.js";
 
+function createSceneButton(name, index, x,y, advtex){
+    var button = BABYLON.GUI.Button.CreateSimpleButton(name, index);
+        button.left = x;
+        button.top  =  y;
+        button.width = "80px"
+        button.height = "30px";
+        button.color = "white";
+        button.cornerRadius = 20;
+        button.background = "green";
+        button.onPointerUpObservable.add(function() {
+            setSceneIndex(index - 1);
+        });
+        advtex.addControl(button); 
+        return button;
+}
+
 export default function guiScene(engine) {
     var guiScene = new BABYLON.Scene(engine);
 
     var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 100, BABYLON.Vector3.Zero(), guiScene);
-   
 
     var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("myUI",true);
 
-
-    var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "1");
-        button1.left = "-100px";
-        button1.top  =  "100px";
-        button1.width = "100px"
-        button1.height = "40px";
-        button1.color = "white";
-        button1.cornerRadius = 20;
-        button1.background = "green";
-        button1.onPointerUpObservable.add(function() {
-            setSceneIndex(0);
-        });
-        advancedTexture.addControl(button1);    
-
-    var button2 = BABYLON.GUI.Button.CreateSimpleButton("but2", "2");
-        button2.left = "000px";
-        button2.top  =  "100px";
-        button2.width = "100px"
-        button2.height = "40px";
-        button2.color = "white";
-        button2.cornerRadius = 20;
-        button2.background = "green";
-        button2.onPointerUpObservable.add(function() {
-            setSceneIndex(1);
-        });
-        advancedTexture.addControl(button2);  
-
-    var button3 = BABYLON.GUI.Button.CreateSimpleButton("but3", "3");
-        button3.left = "100px";
-        button3.top  =  "100px";
-        button3.width = "100px"
-        button3.height = "40px";
-        button3.color = "white";
-        button3.cornerRadius = 20;
-        button3.background = "green";
-        button3.onPointerUpObservable.add(function() {
-            setSceneIndex(2);
-        });
-        advancedTexture.addControl(button3);  
-        //guiScene.debugLayer.show();
+    var button1 = createSceneButton("but1", 1,"-100px", "200px", advancedTexture);
+    var button2 = createSceneButton("but2", 2,"000px", "200px", advancedTexture);
+    var button3 = createSceneButton("but3", 3,"100px", "200px", advancedTexture);
+     
+    //guiScene.debugLayer.show();
     return guiScene;
 }    
